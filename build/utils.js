7c269f952d717e7702e0773d67f41f8fa802c93d
footars <18717808365@163.com>
2019-06-19 00:12:50 +0800

    init

--- build/utils.js
+++ build/utils.js
@@ -8,9 +8,28 @@ exports.assetsPath = function (_path) {
 }
 
 exports.cssLoaders = function (options) {
-
+    const cssLoader = {
+        loader: "css-loader",
+        options: {
+            sourceMap: options.sourceMap
+        }
+    };
+    const sassLoader = {
+        loader: "sass-loader",
+        options: {
+            sourceMap: options.sourceMap
+        }
+    }
+    const postCssLoader = {
+        loader: "sass-loader",
+        options: {
+            sourceMap: options.sourceMap
+        }
+    }
 }
 
 exports.styleLoaders = function (options) {
-
+    if (!Object.is(Object.prototype.toString.call(options),"[object Object]")) return false;
+    const output = [];
+    const loaders = exports.cssLoaders(options);
 }
